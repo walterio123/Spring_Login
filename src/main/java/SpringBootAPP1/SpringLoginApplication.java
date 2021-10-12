b688d7c5f8371993666bfec799288b2b417c6c71
Victor Schweizer <walterio12345@gmail.com>
2021-10-12 10:07:10 -0300

    First commit

---
+++ src/main/java/SpringBootAPP1/SpringLoginApplication.java
@@ -0,0 +1,13 @@
+package SpringBootAPP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginApplication.class, args);
+	}
+
+}
